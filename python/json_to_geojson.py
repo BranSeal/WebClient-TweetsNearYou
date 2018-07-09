0104f349dc0dfd8ad299815ed124bf285153d226
chang.andrew.23@gmail.com <andrew_chang@Andrew-Chang.local>
2018-07-09 01:12:19 -0400

    python script can now iterate through json file

--- python/json_to_geojson.py
+++ python/json_to_geojson.py
@@ -1,4 +1,5 @@
 import json
+import pprint
 import sys
 import os
 from datetime import datetime, timedelta
@@ -21,40 +22,27 @@ def main():
 	end_date = date_obj + timedelta(days=1)
 	tomorrow = end_date.isoformat()
 
-	globalID = data["globalID"]
+	geojson = {'type':'FeatureCollection', 'features':[]}
+	for location in data["list"]:
+		globalID = location["globalID"]
 
-	#calculation of lat/long
-	col = globalID % 10000
-	row = globalID / 10000
-	lon = float(col / 24.0 - 180.0)
-	lat = float(row / 24.0 - 90.0)
+		#calculation of lat/long
+		col = globalID % 10000
+		row = globalID / 10000
+		lon = float(col / 24.0 - 180.0)
+		lat = float(row / 24.0 - 90.0)
+		title = location["rankedList"][0]
+		link = "https://twitter.com/search?q=%23" + title + "%20near%3A" + str(lat) + "%2C" + str(lon) + "%20within%3A2mi%20since%3A" + date + "%20until%3A" + tomorrow + "&src=typd"
+		description = "See corresponding tweets here: <br></br> <a target = '_blank' href = '" + link + "'>" + link + "</a>"
+		feature = {'type':'Feature', 'properties':{}, 'geometry':{'type':'Point', 'coordinates':[]}}
+		feature['geometry']['coordinates'] = [lon,lat]
+		feature['properties'] = {"title": "#" + title, "description": description}
+		geojson['features'].append(feature)
 
-	title = data["rankedList"][0]
-
-	link = "https://twitter.com/search?q=%23" + title + "%20near%3A" + str(lat) + "%2C" + str(lon) + "%20within%3A2mi%20since%3A" + date + "%20until%3A" + tomorrow + "&src=typd"
-	description = "See corresponding tweets here: <br></br> <a target = '_blank' href = '" + link + "'>" + link + "</a>"
-
-	geos = []
-	point = {
-		'type': 'Feature',
-		'properties': {
-			"title": "#" + title,
-			"description": description
-		},
-		"geometry": {
-			"type": "Point",
-			"coordinates": [lon, lat]
-		}
-	}
-
-	toWrite = {
-		"type": "FeatureCollection",
-		"features": [point]
-	}
-
+	#print(geojson)
 	output_filename = os.path.join(dir_name, base_filename + suffixGeoJSON)
 	with open(output_filename, 'wb') as output_file:
-	    json.dump(toWrite, output_file, indent=2)
+	    json.dump(geojson, output_file, indent=2)
 
 if __name__ == "__main__":
     main()
