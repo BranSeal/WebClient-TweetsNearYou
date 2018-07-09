dc2eecb31d0d0bb747bda3946ff50eb890280872
chang.andrew.23@gmail.com <andrew_chang@Andrew-Chang.local>
2018-07-09 18:43:30 -0400

    more merge conflicts

--- js/login.js
+++ js/login.js
@@ -60,7 +60,7 @@ function signIn() {
         // [END authwithemail]
         firebase.auth().onAuthStateChanged(user => {
             if(user) {
-                window.location = 'index.html'; //After successful login, user will be redirected to home.html
+                window.location = 'main.html'; //After successful login, user will be redirected to home.html
             }
         });
     }
